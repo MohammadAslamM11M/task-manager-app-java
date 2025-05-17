1aceb405b75fccbe840a5f867d6f8baec4acdb82
Mohammad Aslam <aslam071595@gmail.com>
2025-05-17 13:51:08 +0530

    Basic backend setup has been implemented

---
+++ backend/src/main/java/com/example/taskmanager/controller/TaskController.java
@@ -0,0 +1,41 @@
+package com.example.taskmanager.controller;
+
+import com.example.taskmanager.model.Task;
+import com.example.taskmanager.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "hhtp://localhost:3000") // allow React frontend
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+    
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @GetMapping
+    public List<Task> getAllTasks(){
+        return taskRepository.findAll();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task){
+        return taskRepository.save(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task taskDetails){
+        Task task = taskRepository.findById(id).orElseThrow();
+        task.setTitle(taskDetails.getTitle());
+        task.setDescription(taskDetails.getDescription());
+        task.setCompleted(taskDetails.isCompleted());
+        return taskRepository.save(task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id){
+        taskRepository.deleteById(id);
+    }
+}
